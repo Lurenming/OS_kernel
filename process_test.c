ab51c305f147f7bc885711db463c7584562105c5
Lurenming <457192436@qq.com>
2025-03-25 02:32:27 +0800

    进程管理（文件输入成功）

--- process_test.c
+++ process_test.c
@@ -5,16 +5,24 @@
 int main() 
 {
     initqueue();
-    PCB* p1=create_process(1, 5, "test1", 1, 2); // pid，优先级，名称，开始时间，运行时间
-    PCB* p2=create_process(2, 4, "test2", 0, 3);
-    PCB* p3=create_process(3, 4, "test3", 1, 5);
-    PCB* p4=create_process(4, 3, "test4", 4, 3);
+    FILE* fp = fopen("process_input.txt", "r");
+    if(fp != NULL) printf("打开文件\"process_input.txt\"成功！\n");
+    else {
+        printf("文件打开失败！\n");
+        exit(0);
+    }
+    int pid, priority, start_time, run_time;
+    char name[20];
+    while(fscanf(fp, "%d %d %s %d %d", &pid, &priority, name, &start_time, &run_time) != EOF){
+        PCB* p = create_process(pid, priority, name, start_time, run_time);// pid，优先级，名称，开始时间，运行时间
+        add_to_queue(p_start_queue, p);
+    }
 
-    add_to_queue(p_start_queue,p1);
-    add_to_queue(p_start_queue,p2);
-    add_to_queue(p_start_queue,p3);
-    add_to_queue(p_start_queue,p4);
-    //print_queue(p_start_queue);
+    printf("请输入你所需要的进程调度算法序号：\n");
+    printf("0 -- FCFS \n1 -- SJF2\n2 -- 优先级\n3 -- RR\n");
+    scanf("%d", &scheNum);
+    printf("请输入算法是否是抢占式的：\n0 -- 非抢占式\n1 -- 抢占式\n");
+    scanf("%d", &mode);
     run();
 
     /*
